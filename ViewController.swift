4b01d4816b2ea589acc4199069753ac00d9067cc
yexinwu <ywu12319@terpmail.umd.edu>
2020-12-08 16:23:25 -0500

    addViewController

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  healthyeat
+//
+//  Created by YEXIN WU on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
